072e2e1271098815e08fd18e8748209bd593438c
XuYanci <grandy.wind@gmail.com>
2018-06-04 18:03:27 +0800

    add runtime project

---
+++ runtime/runtime/runtime/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  runtime
+//
+//  Created by Yanci on 2018/6/4.
+//  Copyright © 2018年 tdw. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ runtime/runtime/runtime/ViewController.m
@@ -0,0 +1,31 @@
+//
+//  ViewController.m
+//  runtime
+//
+//  Created by Yanci on 2018/6/4.
+//  Copyright © 2018年 tdw. All rights reserved.
+//
+
+#import "ViewController.h"
+/// runtime 头文件
+#import <objc/message.h>
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
