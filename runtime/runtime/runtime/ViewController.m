ed5da4b637b470e05afd30ee2fb8fbc671cf7a01
XuYanci <grandy.wind@gmail.com>
2018-06-06 18:08:18 +0800

    commit some

--- runtime/runtime/runtime/ViewController.m
+++ runtime/runtime/runtime/ViewController.m
@@ -10,6 +10,7 @@
 /// runtime 头文件
 #import <objc/message.h>
 
+
 @interface ViewController ()
 
 @end
@@ -21,10 +22,42 @@
     // Do any additional setup after loading the view, typically from a nib.
 }
 
-
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+/// 消息解析
+- (IBAction)methodResolution:(id)sender {
+    
+}
+
+/// 快速向前
+- (IBAction)fastForwarding:(id)sender {
+    
+}
+
+/// 普通向前
+- (IBAction)normalForwarding:(id)sender {
+    
+}
+
+/// 解析方法
++ (BOOL)resolveInstanceMethod:(SEL)sel {
+    return YES;
+}
+
+/// 解析类方法
++ (BOOL)resolveClassMethod:(SEL)sel {
+    return YES;
+}
+
+- (id)forwardingTargetForSelector:(SEL)aSelector {
+    return [super forwardingTargetForSelector:aSelector];
+}
+
+- (void)forwardInvocation:(NSInvocation *)anInvocation {
+    
 }
 
 
