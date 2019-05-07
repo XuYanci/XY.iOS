51148ff1b17ce7f46c2b28eb12ae616070652b88
XuYanci <grandy.wind@gmail.com>
2019-05-07 13:09:32 +0800

    1. 添加block

---
+++ block/Block1/Block1/block1.c
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  Block1
+//
+//  Created by Yanci on 2019/5/7.
+//  Copyright © 2019 Yanci. All rights reserved.
+//
+
+#include <stdio.h>
+int main()
+{
+    ^{ printf("Hello, World!\n"); } ();
+    return 0;
+}
