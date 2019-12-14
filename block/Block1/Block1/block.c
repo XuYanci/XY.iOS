2d91a80c4cd12d17dc72132fcf15c2b1144ecc81
yanci <grandy.wind@gmail.com>
2019-12-14 18:49:32 +0800

    commit some

---
+++ block/Block1/Block1/block.c
@@ -0,0 +1,15 @@
+//
+//  block.c
+//  Block1
+//
+//  Created by Yanci on 2019/12/13.
+//  Copyright © 2019 Yanci. All rights reserved.
+//
+
+#include <stdio.h>
+int main()
+{
+    ^{ printf("Hello, World!\n"); } ();
+    return 0;
+    
+}
--- runloop/runloop/runloop/main.m
+++ runloop/runloop/runloop/main.m
@@ -12,6 +12,6 @@ int main(int argc, const char * argv[]) {
     @autoreleasepool {
         // insert code here...
         NSLog(@"Hello, World!");
-    }
+    }CFRunLoopRef
     return 0;
 }
